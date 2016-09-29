e1947bab5f9e392ca2445fc4acf12b9c2d8350f6
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2016-09-29 18:25:08 +0530

    script.sh: updated after actual usage

--- script.sh
+++ script.sh
@@ -4,6 +4,8 @@
 # config vars
 REPO_NAME=ActozenQC3
 REPO_LOC=/var/repo/${REPO_NAME}
+REPO_GIT=${REPO_LOC}/.git
+CODECEPT_CONF=${REPO_LOC}/codeception.yml
 SCRIPT_OUTPUT=/tmp/CInner_run_${REPO_NAME}_$(date +%s).txt
 COMMIT=$1
 
@@ -17,20 +19,20 @@ fi
 
 
 # checkout specified commit
-cd "${GIT_REPO}"
-git fetch origin "${COMMIT}"
-git checkout "${COMMIT}"
+#cd "${GIT_REPO}"
+git --git-dir=${REPO_GIT} --work-tree=${REPO_LOC} fetch origin "${COMMIT}" --quiet
+git --git-dir=${REPO_GIT} --work-tree=${REPO_LOC} checkout "${COMMIT}" --quiet
 
 # pre-run tasks
 
 # set status as pending
 
 # run specified script
-${REPO_LOC}/vendor/bin/codecept run api --quiet --no-colors > "${SCRIPT_OUTPUT}" 2>&1
+${REPO_LOC}/vendor/bin/codecept run api --no-colors --config ${CODECEPT_CONF} > "${SCRIPT_OUTPUT}" 2>&1
 
 # parse output from script
 
 # set final status
 
 # revert to master branch
-git checkout master
+git --git-dir=${REPO_GIT} --work-tree=${REPO_LOC} checkout master --quiet
