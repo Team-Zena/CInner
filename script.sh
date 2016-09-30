e10f216c91d868797988a57d4283a610925515e5
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2016-09-30 11:15:09 +0530

    script.sh: added command line argument parsing

--- script.sh
+++ script.sh
@@ -8,21 +8,53 @@ REPO_GIT=${REPO_LOC}/.git
 CODECEPT_CONF=${REPO_LOC}/codeception.yml
 SCRIPT_OUTPUT_DIR=/var/log/cinner
 SCRIPT_OUTPUT=${SCRIPT_OUTPUT_DIR}/CInner_run_${REPO_NAME}_$(date +%s).log
-COMMIT=$1
+QUIET=" --quiet "
 
-# check cmd line
+# functions
+show_help() {
+cat << EOF
+Usage: ${0##*/} [-hv] [-c COMMIT]
+Fetches the commit with specified hash, runs the test and posts the output.
+
+    -h          display this help and exit
+    -c COMMIT   write the result to OUTFILE instead of standard output.
+    -v          verbose mode.
+EOF
+}
+
+# cmd line args
+while getopts ":c:vh" opt; do
+  case $opt in
+    c)
+        COMMIT=$OPTARG
+      ;;
+    v)
+        QUIET=""
+      ;;
+    h)
+        show_help
+        exit 0
+      ;;
+    \?)
+        show_help >&2
+        exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+# check variables
 if [ -z ${COMMIT} ]; then
-	echo "commit to checkout not specified!"
-	exit 1
+        echo "commit to checkout not specified!"
+        exit 1
 fi
 
-# other checks
-
-
 # checkout specified commit
-#cd "${GIT_REPO}"
-git --git-dir=${REPO_GIT} --work-tree=${REPO_LOC} fetch origin "${COMMIT}" --quiet
-git --git-dir=${REPO_GIT} --work-tree=${REPO_LOC} checkout "${COMMIT}" --quiet
+git --git-dir=${REPO_GIT} --work-tree=${REPO_LOC} fetch origin "${COMMIT}" ${QUIET} #|| exit 1
+git --git-dir=${REPO_GIT} --work-tree=${REPO_LOC} checkout "${COMMIT}" ${QUIET} #|| exit 1
 
 # pre-run tasks
 
@@ -43,4 +75,4 @@ fi
 # set final status
 
 # revert to master branch
-git --git-dir=${REPO_GIT} --work-tree=${REPO_LOC} checkout master --quiet
+git --git-dir=${REPO_GIT} --work-tree=${REPO_LOC} checkout master ${QUIET}
